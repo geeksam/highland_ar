f52dd3f9fac95023487279fbd287fd28391d398d
Sam Livingston-Gray <geeksam@gmail.com>
2023-09-07 20:05:53 -0700

    cancel the TERF lady
    This code doesn't even run any more, but I do come back to look at it from time
    to time, and I'm tired of being reminded of that awful person.

--- test/highland_ar_test.rb
+++ test/highland_ar_test.rb
@@ -5,12 +5,12 @@ class HighlandARModuleTest < HighlandARTestCase
     assert_nothing_raised(NameError) { HighlandAR }
     assert_kind_of(Module, HighlandAR)
   end
-  
+
   test "module gives includers a .there_can_be_only_one macro" do
     Platypus = new_highlandar_class
     assert_nothing_raised(NoMethodError) { Platypus.there_can_be_only_one :bill }
   end
-  
+
   test "calling the macro invokes .has_one" do
     Alpaca = new_highlandar_class
     Alpaca.expects(:has_one).with(:spitoon)
@@ -38,56 +38,56 @@ class TCBOO_SetterTest < HighlandARTestCase
     include HighlandAR
     there_can_be_only_one :champion
   end
-  
+
   def setup
     @arena = Arena.new
   end
-  
+
   test "single contestant is automatically selected -- argument as object" do
     @arena.expects(:tcboo_immortal_combat).never
-    @arena.champion = 'Fred'
-    assert_equal('Fred', @arena.champion)
+    @arena.champion = 'Frodo'
+    assert_equal('Frodo', @arena.champion)
   end
-  
+
   test "single contestant is automatically selected -- argument as array" do
     @arena.expects(:tcboo_immortal_combat).never
-    @arena.champion = %w[Fred]
-    assert_equal('Fred', @arena.champion)
+    @arena.champion = %w[Frodo]
+    assert_equal('Frodo', @arena.champion)
   end
-  
+
   test "champion selection functions as a playoff tree, two entrants" do
-    @arena.expects(:tcboo_shuffle_combatants).with(%w[Fred George]).returns(%w[Fred George])
-    @arena.expects(:tcboo_immortal_combat).with('Fred', 'George').returns('George')
-    @arena.champion = %w[Fred George]
-    assert_equal('George', @arena.champion)
+    @arena.expects(:tcboo_shuffle_combatants).with(%w[Frodo Gimli]).returns(%w[Frodo Gimli])
+    @arena.expects(:tcboo_immortal_combat).with('Frodo', 'Gimli').returns('Gimli')
+    @arena.champion = %w[Frodo Gimli]
+    assert_equal('Gimli', @arena.champion)
   end
-  
+
   test "champion selection functions as a playoff tree, three entrants" do
     # Round 1
-    @arena.expects(:tcboo_shuffle_combatants).with(%w[Fred George Ron]).returns(%w[Fred George Ron])
-    @arena.expects(:tcboo_immortal_combat).with('Fred', 'George').returns('George')
-    @arena.expects(:tcboo_immortal_combat).with('Ron', nil).returns('Ron')
+    @arena.expects(:tcboo_shuffle_combatants).with(%w[Frodo Gimli Elrond]).returns(%w[Frodo Gimli Elrond])
+    @arena.expects(:tcboo_immortal_combat).with('Frodo', 'Gimli').returns('Gimli')
+    @arena.expects(:tcboo_immortal_combat).with('Elrond', nil).returns('Elrond')
 
     # Round 2
-    @arena.expects(:tcboo_shuffle_combatants).with(%w[George Ron]).returns(%w[George Ron])
-    @arena.expects(:tcboo_immortal_combat).with('George', 'Ron').returns('George')
+    @arena.expects(:tcboo_shuffle_combatants).with(%w[Gimli Elrond]).returns(%w[Gimli Elrond])
+    @arena.expects(:tcboo_immortal_combat).with('Gimli', 'Elrond').returns('Gimli')
 
-    @arena.champion = %w[Fred George Ron]
-    assert_equal('George', @arena.champion)
+    @arena.champion = %w[Frodo Gimli Elrond]
+    assert_equal('Gimli', @arena.champion)
   end
-  
+
   test "champion selection functions as a playoff tree, four entrants" do
     # Round 1
-    @arena.expects(:tcboo_shuffle_combatants).with(%w[Fred George Ron Harry]).returns(%w[Fred George Ron Harry])
-    @arena.expects(:tcboo_immortal_combat).with('Fred', 'George').returns('Fred')
-    @arena.expects(:tcboo_immortal_combat).with('Ron', 'Harry').returns('Hermione')  # just making sure you're paying attention
+    @arena.expects(:tcboo_shuffle_combatants).with(%w[Frodo Gimli Elrond Luke]).returns(%w[Frodo Gimli Elrond Luke])
+    @arena.expects(:tcboo_immortal_combat).with('Frodo', 'Gimli').returns('Frodo')
+    @arena.expects(:tcboo_immortal_combat).with('Elrond', 'Luke').returns('Gandalf')  # just making sure you're paying attention
 
     # Round 2
-    @arena.expects(:tcboo_shuffle_combatants).with(%w[Fred Hermione]).returns(%w[Fred Hermione])
-    @arena.expects(:tcboo_immortal_combat).with('Fred', 'Hermione').returns('Hermione') # obviously
+    @arena.expects(:tcboo_shuffle_combatants).with(%w[Frodo Gandalf]).returns(%w[Frodo Gandalf])
+    @arena.expects(:tcboo_immortal_combat).with('Frodo', 'Gandalf').returns('Gandalf') # obviously
 
-    @arena.champion = %w[Fred George Ron Harry]
-    assert_equal('Hermione', @arena.champion)
+    @arena.champion = %w[Frodo Gimli Elrond Luke]
+    assert_equal('Gandalf', @arena.champion)
   end
 
   test "champion selection functions as a playoff tree, nine entrants" do
@@ -98,31 +98,31 @@ class TCBOO_SetterTest < HighlandARTestCase
     @arena.expects(:tcboo_immortal_combat).with('E', 'F').returns('E')
     @arena.expects(:tcboo_immortal_combat).with('G', 'H').returns('G')
     @arena.expects(:tcboo_immortal_combat).with('I', nil).returns('I')
-  
+
     # Round 2
     @arena.expects(:tcboo_shuffle_combatants).with(%w[A C E G I]).returns(%w[A C E G I])
     @arena.expects(:tcboo_immortal_combat).with('A', 'C').returns('C')
     @arena.expects(:tcboo_immortal_combat).with('E', 'G').returns('G')
     @arena.expects(:tcboo_immortal_combat).with('I', nil).returns('I')  # CGI. Get it? CGI! ... sheesh. tough room.
-  
+
     # Round 3
     @arena.expects(:tcboo_shuffle_combatants).with(%w[C G I]).returns(%w[C G I])
     @arena.expects(:tcboo_immortal_combat).with('C', 'G').returns('G')
     @arena.expects(:tcboo_immortal_combat).with('I', nil).returns('I')  # Knowing is half the battle.
-    
+
     # Round 4
     @arena.expects(:tcboo_shuffle_combatants).with(%w[G I]).returns(%w[G I])
     @arena.expects(:tcboo_immortal_combat).with('G', 'I').returns('I')  # Co-champions: 'Me', 'Myself'.
-  
+
     @arena.champion = %w[A B C D E F G H I]
     assert_equal('I', @arena.champion)
   end
-  
+
   test "this 'test' (a) is random and (b) has no assertions; I include it solely for the amusing side effects" do
     begin
       puts ''
       $debug = true
-      @arena.champion = %w[Harry Ron Hermione Hagrid Dumbledore Snape RandomBystander Tweedledee Alice]
+      @arena.champion = %w[Luke Elrond Gandalf Obi-Wan Magneto Ganondorf RandomBystander Tweedledee Alice]
     ensure
       $debug = false
     end
